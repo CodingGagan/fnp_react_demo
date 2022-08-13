dbecb100bd66c01b206274a82a7a018bc59e7cb1
Yaseen Rashid <yaseen.rashid@whistle.mobi>
2022-08-13 16:59:15 +0530

    checkout complete

--- src/component/Checkout/checkout_first.jsx
+++ src/component/Checkout/checkout_first.jsx
@@ -9,15 +9,15 @@ export default function CheckoutFirst() {
                 <div className='left-main d-flex flex-column '>
 
                     <div className='d-flex justify-content-between bg-secondary-light p-2'>
-                        <div className='align-self-center text-white'><label className='bg-dark p-1 rounded'>1</label></div>
-                        <div className='align-self-center'><label htmlFor=""><strong>JOHN DOE</strong></label></div>
-                        <div className='align-self-center'><label htmlFor="">JOHNDOE@EXAMPLE.COM</label></div>
+                        <div className='align-self-center text-white'><p className='bg-dark p-1 rounded'>1</p></div>
+                        <div className='align-self-center'><p htmlFor=""><strong>JOHN DOE</strong></p></div>
+                        <div className='align-self-center'><p htmlFor="">JOHNDOE@EXAMPLE.COM</p></div>
                     </div>
 
                     <div className='d-flex  bg-secondary-light my-1 p-2 flex-column'>
                         <div className='d-flex justify-content-between '>
-                            <div className='text-white '><label className='bg-dark p-1 rounded'>2</label></div>
-                            <div className=''><label htmlFor=""><strong>Order And Delivery Details</strong></label></div>
+                            <div className='text-white '><p className='bg-dark p-1 rounded'>2</p></div>
+                            <div className=''><p htmlFor=""><strong>Order And Delivery Details</strong></p></div>
                         </div>
 
                         <div className='d-flex justify-content-between p-2'>
@@ -26,36 +26,31 @@ export default function CheckoutFirst() {
                                 <img className="img-thumbnail-custom rounded" src="https://img.freepik.com/free-photo/top-view-beautiful-roses-bouquet-with-pink-ribbon_23-2148822309.jpg?w=360&t=st=1659981086~exp=1659981686~hmac=58a30a6d203b3378ffda9121b0773b2f882101eb49d1c477ec49bc1129fc1c14" alt="photo" />
                             </div>
                             <div className='align-self-center m-2'>
-                                <h6 htmlFor="">Frosty Chocolate Cake</h6>
-                                <h6 htmlFor="">Rs 595 * 1Round 1/2 KG</h6>
-                                <h6 htmlFor="">Frosty Chocolate Cake</h6>
-                                <h6 htmlFor="">Eggless</h6>
-                                <h6 htmlFor="">Chocolate Flavour</h6>
+                                <p htmlFor="">Frosty Chocolate Cake</p>
+                                <p htmlFor="">Rs 595 * 1Round 1/2 KG</p>
+                                <p htmlFor="">Eggless</p>
+                                <p htmlFor="">Chocolate Flavour</p>
                             </div>
                             <div className='align-self-center'>
-                                <h6 htmlFor="">Delivery ON</h6>
-                                <h6 htmlFor=""><strong>Sat, 7 Aug, 2022</strong></h6>
-                                <h6 htmlFor="">Standard Delivery</h6>
-                                <h6 htmlFor="">  08:00 - 15:00 hrs</h6>
+                                <p htmlFor="">Delivery ON</p>
+                                <p htmlFor=""><strong>Sat, 7 Aug, 2022</strong></p>
+                                <p htmlFor="">Standard Delivery</p>
+                                <p htmlFor="">  08:00 - 15:00 hrs</p>
                             </div>
                             <div className='align-self-center btn bg-dark text-white'>change</div>
                         </div>
                     </div>
                     <div className='d-flex flex-column w-25 m-1'>
-                        <label htmlFor="">Message on Cake</label>
+                        <p htmlFor="">Message on Cake</p>
                         <input type="text" name="" id="" />
                     </div>
                 </div>
-
-
-
-
             </Col>
             <Col>
                 <div className='right-main d-flex flex-column bg-secondary-light p-3'>
                     <div className=''>
                         <div>
-                            <label htmlFor=""><strong>Price Details</strong></label>
+                            <p htmlFor=""><strong>Price Details</strong></p>
                         </div>
                     </div>
                     <div className=''>
