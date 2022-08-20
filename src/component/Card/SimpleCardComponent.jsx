a8a20500ccd5530a469053d69cd9e6c0eb825ceb
Yaseen Rashid <yaseen.rashid@whistle.mobi>
2022-08-20 17:40:45 +0530

    check

---
+++ src/component/Card/SimpleCardComponent.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import './CardComponent.css';
+import { Link } from 'react-router-dom';
+export default function SimpleCardComponent({ cardClass, source, showContent, value, cardContent, custContentImg, custContent, checker }) {
+    return (
+        <>
+            <div class="card justify-content-center bg-transparent border-0 text-center">
+                <img class="card-img-top  card-custom" src={source} alt="Card image cap" />
+                {custContentImg && checker == 0 ? <div className="customCenterContent"><h2>{custContent}</h2></div> : ''}
+                {
+                    showContent ?
+                        <>
+                            {checker != 0 ? <Card.Body>
+                                <Card.Title>{cardContent}</Card.Title>
+                                <Card.Text>Rs {value}</Card.Text>
+                            </Card.Body> : ''}
+                        </>
+                        :
+                        <>
+                            {checker != 0 ? <div className='cardContent-new'>
+                                {cardContent}
+                            </div> : ''}
+                        </>
+                }
+                           
+
+            </div>
+            
+        </>
+    )
+}
