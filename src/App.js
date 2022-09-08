2b26c3577f7ed86faa269eb4d107d623387abbef
Yaseen Rashid <yaseen.rashid@whistle.mobi>
2022-09-08 22:33:39 +0530

    added some new files

--- src/App.js
+++ src/App.js
@@ -1,27 +1,31 @@
 
 import './App.css';
 import MainNavbar from './layout/MainNavbar';
-import Footer from './layout/Footer';
+// import Footer from './layout/Footer';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './pages/home';
 import ProductDeatils from './pages/product_details';
 import CheckoutDeatils from './pages/checkout_details';
+import MainCategory from './pages/Admin/Categories/main_category';
+import SubCategory from './pages/Admin/Categories/sub_category';
+import AdminSidebar from './pages/Admin/Layout/admin_sidebar';
+
 import Login from './pages/Login';
 import Sidebar from './layout/Sidebar/Sidebar';
 import {
   Route, Routes
 } from "react-router-dom";
-import Sub_Footer from './layout/Sub_Footer';
+// import Sub_Footer from './layout/Sub_Footer';
 import { useState } from 'react';
 import Backdrop from './layout/Backdrop/Backdrop';
 import Category from './pages/category';
 import ProductListing from './pages/product_listing';
 import Cart from './component/Cart/Cart';
+import AdminHeader from './pages/Admin/Layout/admin_header';
 // import SweetAlert from 'react-bootstrap-sweetalert';
 
 function App() {
   const [toggle, setToggle] = useState(false);
-  var check = false;
 
   const drawerToggleClickHandler = () => {
     console.log("CLicked " + toggle);
@@ -65,7 +69,7 @@ function App() {
   // }
   return (
     <>
-      <MainNavbar cartOpen={drawerToggleClickHandler} />
+      {/* <MainNavbar cartOpen={drawerToggleClickHandler} /> */}
       <Sidebar cartCount={cartCount} show={toggle} />
       {backdrop}
       {/* <Container fluid direction="horizontal" gap={3}> */}
@@ -78,6 +82,10 @@ function App() {
         <Route path="/checkout_details" element={<CheckoutDeatils />}></Route>
         <Route path="/product_details" element={<ProductDeatils />}></Route>
         <Route path="/checkout_details" element={<CheckoutDeatils />}></Route>
+        <Route path="/Admin/Layout/admin_header" element={<AdminHeader />}></Route>
+        <Route path="/Admin/Layout/admin_sidebar" element={<AdminSidebar />}></Route>
+        <Route path="/Admin/Categories/main_category" element={<MainCategory />}></Route>
+        <Route path="/Admin/Categories/sub_category" element={<SubCategory />}></Route>
       </Routes>
     </>
   );
